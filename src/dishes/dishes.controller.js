973c9e49f122a95dba1230561f83de55956a6e3d
Johnny Faris <jkfaris@gmail.com>
2025-05-01 14:00:37 -0700

    read function is passing, used dishExist to filter and check if dish id is found in data and store it locally

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -11,6 +11,28 @@ function list(req, res) {
     res.json({ data: dishes });
   }
 
+
+  function dishExists(req, res, next) {
+    const { dishId } = req.params;
+    const foundDish = dishes.find((dish) => dish.id === dishId);
+  
+    if (!foundDish) {
+      return next({
+        status: 404,
+        message: `Dish ID not found: ${dishId}`,
+      });
+    }
+  
+    res.locals.dish = foundDish;
+    next();
+  }
+
+    function read(req, res) {
+    res.json({ data: res.locals.dish });
+    }
+
   module.exports = {
     list,
+    read,
+    dishExists,
   };
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -5,6 +5,6 @@ const controller = require("./dishes.controller");
 // TODO: Implement the /dishes routes needed to make the tests pass
 router.get("/", controller.list);
 
-router.get("/:dishId", controller.read);
+router.get("/:dishId", controller.dishExists, controller.read);
 
 module.exports = router;
