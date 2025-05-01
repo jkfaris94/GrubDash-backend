03f5a41a7053fcdc204fb07605c94ed69d8212f2
Johnny Faris <jkfaris@gmail.com>
2025-05-01 14:52:24 -0700

    ./:orderId is passing read tests

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -7,3 +7,32 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+function list(req, res) {
+    res.json({ data: orders });
+  }
+
+function orderExists(req, res, next) {
+  const { orderId } = req.params;
+  const foundOrder = orders.find((order) => order.id === orderId);
+
+  if (!foundOrder) {
+    return next({
+      status: 404,
+      message: `Order ID not found: ${orderId}`,
+    });
+  }
+
+  res.locals.order = foundOrder;
+  next();
+}
+
+function read(req, res) {
+    res.json({ data: res.locals.order });
+  }
+
+
+module.exports = {
+    list,
+    read,
+    orderExists,
+};
