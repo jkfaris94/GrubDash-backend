d9c9da9e4c267e79b88a4c8971f6dd15d51d20db
Johnny Faris <jkfaris@gmail.com>
2025-05-01 18:18:45 -0700

    orders create method passing all tests

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -30,9 +30,58 @@ function read(req, res) {
     res.json({ data: res.locals.order });
   }
 
+  function validateOrder(req, res, next) {
+    const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+  
+    if (!deliverTo || deliverTo === "") {
+      return next({ status: 400, message: "Order must include a deliverTo" });
+    }
+  
+    if (!mobileNumber || mobileNumber === "") {
+      return next({ status: 400, message: "Order must include a mobileNumber" });
+    }
+  
+    if (!Array.isArray(dishes) || dishes.length === 0) {
+        return next({ status: 400, message: "Order must include at least one dish" });
+    }
+  
+    for (let i = 0; i < dishes.length; i++) {
+      const quantity = dishes[i].quantity;
+  
+      if (
+        quantity === undefined ||
+        !Number.isInteger(quantity) ||
+        quantity <= 0
+      ) {
+        return next({
+          status: 400,
+          message: `Dish ${i} must have a quantity that is an integer greater than 0`,
+        });
+      }
+    }
+  
+    next();
+  }
+
+  function create(req, res) {
+    const { data: { deliverTo, mobileNumber, status = "pending", dishes } = {} } = req.body;
+  
+    const newOrder = {
+      id: nextId(),
+      deliverTo,
+      mobileNumber,
+      status,
+      dishes,
+    };
+  
+    orders.push(newOrder);
+    res.status(201).json({ data: newOrder });
+  }
 
 module.exports = {
     list,
     read,
     orderExists,
+    validateOrder,
+    create,
 };
